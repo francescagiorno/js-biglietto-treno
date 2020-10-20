d498b9e32653ba326a3f4e9bbb0ba6a5fe935435
francesca-giorno <francescagiorno1996@gmail.com>
2020-10-20 16:34:15 +0200

    add js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var chilometriUtente = prompt("Quanti km vuole percorrere?");
+console.log(chilometriUtente);
+
+var etaUtente = prompt("Quanti anni ha?");
+console.log(etaUtente);
+
+var prezzoBiglietto = 0.21;
+
+if (etaUtente < 18){
+  alert(" ha a uno sconto del 20%");
+  document.getElementById("prezzo").innerHTML=("il prezzo del biglietto è di : " + ((prezzoBiglietto * chilometriUtente)*0.8));
+
+} else if (etaUtente > 65){
+
+  allert("ha a uno scont del 40%");
+  document.getElementById("prezzo").innerHTML=("il prezzo del biglietto è di :  " + ((prezzoBiglietto * chilometriUtente)*0.6));
+
+}
