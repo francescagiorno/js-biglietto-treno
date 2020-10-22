5b51ac1a21634d6953c434b2acb8aa50f6aecf13
francesca-giorno <francescagiorno1996@gmail.com>
2020-10-22 09:30:05 +0200

    prove

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-var chilometriUtente = prompt("Quanti km vuole percorrere?");
+/*var chilometriUtente = prompt("Quanti km vuole percorrere?");
 console.log(chilometriUtente);
 
 var etaUtente = prompt("Quanti anni ha?");
-console.log(etaUtente);
-
+console.log(etaUtente);*/
+/*var etaUtente = document.getElementById("FasciaEtà");
+var chilometriUtente = document.getElementById("kmPercorrere");
+var underAge = document.getElementById("minorenne");
+var overAge = document.getElementById("over-65");
 var prezzoBiglietto = 0.21;
+var sconto20 = 0.2;
+var sconto40 = 0.4;*/
 
-if (etaUtente < 18){
+/*if (etaUtente < 18){
   alert(" ha a uno sconto del 20%");
   document.getElementById("prezzo").innerHTML=("il prezzo del biglietto è di : " + ((prezzoBiglietto * chilometriUtente)*0.8));
 
@@ -17,4 +22,13 @@ if (etaUtente < 18){
 
 } else {
   document.getElementById("prezzo").innerHTML=("il prezzo del bigletto è di :" + (prezzoBiglietto * chilometriUtente));
-}
+}*/
+var prezzoBiglietto = 0.21;
+var sconto20 = 0.2;
+var sconto40 = 0.4;
+
+var generaBtn = document.getElementById("generate");
+
+generaBtn.addEventListener("click", function()){
+
+  v
